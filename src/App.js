0fe4113ce803b96e2103a40d9e225c32922364d7
parvez1203 <parvezmir8332@gmail.com>
2024-10-29 06:56:25 +0000

    new test code

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
-import {Elements} from '@stripe/react-stripe-js';
-import {loadStripe} from '@stripe/stripe-js';
+import React from 'react';
+import { Elements } from '@stripe/react-stripe-js';
+import { loadStripe } from '@stripe/stripe-js';
 import CheckoutForm from './components/CheckoutForm';
 
 // Make sure to call `loadStripe` outside of a component’s render to avoid
@@ -8,6 +9,8 @@ const stripePromise = loadStripe('pk_test_51QBRpFICvmtDwewB0gmIXj01IEfYCvQYUEenH
 
 export default function App() {
   return (
-    <CheckoutForm />
+    <Elements stripe={stripePromise}>
+      <CheckoutForm />
+    </Elements>
   );
-};
+}
